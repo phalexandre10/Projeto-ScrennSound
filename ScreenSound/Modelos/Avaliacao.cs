6ba9b7cd9dcab41a5c9fb2e25fed548c0a3dd7c3
Pedro Henrique Alexandre <121180098+phalexandre10@users.noreply.github.com>
2024-05-29 12:22:48 -0300

    Adicionando os arquivos

---
+++ ScreenSound/Modelos/Avaliacao.cs
@@ -0,0 +1,19 @@
+﻿namespace ScreenSound.Modelos;
+
+internal class Avaliacao
+{
+    //Criei a classe avaliação para encapslular o conceito nota
+    public Avaliacao(int nota)
+    {
+        Nota = nota;
+    }
+
+    public int Nota { get; }
+
+    //Usando método estático
+    public static Avaliacao Parse(string texto)
+    {
+        int nota = int.Parse(texto);
+        return new Avaliacao(nota);
+    }
+}
